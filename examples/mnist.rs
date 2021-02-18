848009778f59bf7458887b6e8674a53830da9e2c
whynoodle <119311098+whynoodle@users.noreply.github.com>
2021-02-18 15:22:49 +0000

    Small documentation improvements

--- examples/mnist.rs
+++ examples/mnist.rs
@@ -5,4 +5,8 @@ use rust_rl::network::nn::NeuralNetwork;
 use std::time::Instant;
 
 fn new() -> NeuralNetwork {
-    let mut nn = N
+    let mut nn = NeuralNetwork::new3d((1, 28, 28), "cce".to_string(), "adam".to_string());
+    nn.set_batch_size(32);
+    nn.set_learning_rate(1e-3);
+    nn.add_convolution((3, 3), 10, 1);
+    nn.add_flatt
