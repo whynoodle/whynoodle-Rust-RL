9cedccaed6513044709cf6f5aeb8e6adb4b1627e
whynoodle <119311098+whynoodle@users.noreply.github.com>
2021-02-19 06:13:34 +0000

    Clean up some code.

--- examples/mnist.rs
+++ examples/mnist.rs
@@ -9,4 +9,12 @@ fn new() -> NeuralNetwork {
     nn.set_batch_size(32);
     nn.set_learning_rate(1e-3);
     nn.add_convolution((3, 3), 10, 1);
-    nn.add_flatt
+    nn.add_flatten();
+    nn.add_activation("sigmoid");
+    nn.add_dropout(0.5);
+    nn.add_dense(10);
+    nn.add_activation("softmax");
+    nn
+}
+
+fn test(nn: &mut NeuralNetwork, input: &Array4<f32>, feedback: 
