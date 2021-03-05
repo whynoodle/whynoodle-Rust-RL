a664ee50d947251a8291afe914c5642968aa51ba
whynoodle <119311098+whynoodle@users.noreply.github.com>
2021-03-05 22:07:46 +0000

    Fix markdown typo

--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,9 @@
 /// A submodule including everything to build a neural network.
 ///
 /// Currently multiple layers, error functions, and optimizers are provided.
+pub mod network;
+
+/// A submodule including everything to run reinforcement learning tasks.
+///
+/// This submodule includes two interfaces for environments and agents.
+/// One environment and multiple 
