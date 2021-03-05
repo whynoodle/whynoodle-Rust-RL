9ce6055e1be10fe995cee835b63011e09ffa7384
whynoodle <119311098+whynoodle@users.noreply.github.com>
2021-03-05 14:02:17 +0000

    chore(package): update jsdoc to version 3.4.1

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
 #![deny(missing_docs)]
 
-//! A crate including two submodules for neur
+//! A crate including two submodules for neural networks and reinforcement learning.
+
+/// A submodule including everything to build a neural network.
+///
+/// Currently multiple layers, error functions, and optimizers are provided.
