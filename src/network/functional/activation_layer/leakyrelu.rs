1d91de1c76b20c09e726353ef874d7833c933bf5
whynoodle <119311098+whynoodle@users.noreply.github.com>
2021-03-07 14:21:17 +0000

    get_dvb_firmware: fix download site for tda10046 firmware

--- src/network/functional/activation_layer/leakyrelu.rs
+++ src/network/functional/activation_layer/leakyrelu.rs
@@ -1,3 +1,12 @@
 use crate::network::functional::Functional;
 use crate::network::layer::Layer;
-use nd
+use ndarray::ArrayD;
+
+/// A leaky-relu layer.
+#[derive(Default)]
+pub struct LeakyReLuLayer {}
+
+impl LeakyReLuLayer {
+    /// No parameters are possible.
+    pub fn new() -> Self {
+        Le
