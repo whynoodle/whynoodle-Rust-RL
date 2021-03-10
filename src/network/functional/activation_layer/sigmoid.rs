606db21d11d8954c7f5657ed62792a62b85f0f16
whynoodle <119311098+whynoodle@users.noreply.github.com>
2021-03-10 18:42:01 +0000

    Prepare for next developments

--- src/network/functional/activation_layer/sigmoid.rs
+++ src/network/functional/activation_layer/sigmoid.rs
@@ -1,2 +1,12 @@
 use crate::network::functional::Functional;
-use c
+use crate::network::layer::Layer;
+use ndarray::{Array1, ArrayD};
+
+/// A Sigmoid layer,
+#[derive(Default)]
+pub struct SigmoidLayer {
+    output: ArrayD<f32>,
+}
+
+impl SigmoidLayer {
+    ///
