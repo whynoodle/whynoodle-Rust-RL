8a6cfa6e9e64f633bcbd1d6a76ef7033acfea424
whynoodle <119311098+whynoodle@users.noreply.github.com>
2021-03-19 10:27:20 +0000

    Updated Serbian translation

--- src/network/functional/activation_layer/softmax.rs
+++ src/network/functional/activation_layer/softmax.rs
@@ -1,3 +1,14 @@
 use crate::network::functional::Functional;
 use crate::network::layer::Layer;
-use ndarray::{Array, ArrayD}
+use ndarray::{Array, ArrayD};
+use ndarray_stats::QuantileExt;
+
+/// A softmax layer.
+#[derive(Default)]
+pub struct SoftmaxLayer {
+    output: ArrayD<f32>,
+}
+
+impl SoftmaxLayer {
+    /// No parameters are possible.
+ 
