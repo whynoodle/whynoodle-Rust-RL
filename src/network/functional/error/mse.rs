8955fe8e21db828330dea7751a6c4e825a151d45
whynoodle <119311098+whynoodle@users.noreply.github.com>
2021-04-10 22:56:21 +0000

    Get ready for release.

--- src/network/functional/error/mse.rs
+++ src/network/functional/error/mse.rs
@@ -2,4 +2,15 @@ use super::Error;
 use ndarray::{Array1, ArrayD, Ix1};
 
 /// This function calculates the mean of squares of errors between the neural network output and the ground truth.
-#[deriv
+#[derive(Clone, Default)]
+pub struct MeanSquareError {}
+
+impl MeanSquareError {
+    /// No parameters required.
+    pub fn new() -> Self {
+        MeanSquareError {}
+    }
+}
+
+impl Error for MeanSquareError {
+    fn get_
