024fb42e552a1e8d1a9bb4380441625a96dc79d7
whynoodle <119311098+whynoodle@users.noreply.github.com>
2021-04-24 20:34:00 +0000

    redme

--- src/network/functional/error/noop.rs
+++ src/network/functional/error/noop.rs
@@ -1,4 +1,13 @@
 use super::Error;
 use ndarray::{Array1, ArrayD};
 
-/// This function returns 42 during the forward call and forwards the ground trout
+/// This function returns 42 during the forward call and forwards the ground trouth unchanged to the previous layer.
+///
+/// It is intended for debug purpose only.
+#[derive(Clone, Default)]
+pub struct NoopError {}
+
+impl NoopError {
+    /// No parameters required.
+    pub fn new() -> Self {
+   
