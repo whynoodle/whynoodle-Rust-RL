4bbd486ddb426799698ee8e5f062ee04949a2f32
whynoodle <119311098+whynoodle@users.noreply.github.com>
2021-04-30 06:17:07 +0000

    Update homepage URL

--- src/network/functional/error/rmse.rs
+++ src/network/functional/error/rmse.rs
@@ -1,1 +1,10 @@
-use super::Err
+use super::Error;
+use ndarray::{Array1, ArrayD, Ix1};
+
+/// This error function works on the square-root of the mse.
+#[derive(Clone, Default)]
+pub struct RootMeanSquareError {
+    err: f32,
+}
+
+impl RootMeanSquareE
