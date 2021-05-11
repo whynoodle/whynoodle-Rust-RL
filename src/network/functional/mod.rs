553896cd9cb8050e250c0ec99351c1382bd8a5dd
whynoodle <119311098+whynoodle@users.noreply.github.com>
2021-05-11 15:22:33 +0000

    v1.11

--- src/network/functional/mod.rs
+++ src/network/functional/mod.rs
@@ -1,2 +1,9 @@
 /// This trait forces functional layers and functions to implement Send and Sync
 mod functional_trait;
+
+pub use functional_trait::Functional;
+
+/// This module contains the most common activation functions like sigmoid, relu, or softmax.
+pub mod activation_layer;
+
+/// This submodule provides 
