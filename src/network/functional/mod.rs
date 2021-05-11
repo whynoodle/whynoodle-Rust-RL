8796b136dd915f34e9b83b0f1c17f3f6f044d767
whynoodle <119311098+whynoodle@users.noreply.github.com>
2021-05-11 22:03:20 +0000

    Fix lint error

--- src/network/functional/mod.rs
+++ src/network/functional/mod.rs
@@ -6,4 +6,7 @@ pub use functional_trait::Functional;
 /// This module contains the most common activation functions like sigmoid, relu, or softmax.
 pub mod activation_layer;
 
-/// This submodule provides 
+/// This submodule provides various error function.
+///
+/// Beside of the normal forward() and backward() functions some Error functions implement both functions in a *_from_logits() variant.  
+/// They merge the
