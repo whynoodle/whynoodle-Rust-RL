04bbd6e1064458820a040773f87f62dd73b0f944
whynoodle <119311098+whynoodle@users.noreply.github.com>
2021-05-29 14:38:13 +0000

    removing dead code

--- src/network/functional/mod.rs
+++ src/network/functional/mod.rs
@@ -9,4 +9,6 @@ pub mod activation_layer;
 /// This submodule provides various error function.
 ///
 /// Beside of the normal forward() and backward() functions some Error functions implement both functions in a *_from_logits() variant.  
-/// They merge the
+/// They merge their own implementation with the previous activation function in a numerically more stable way.  
+/// Better known examples are Softmax+CategoricalCrossEntropy or Sigmoid+BinaryCrossEntropy.  
+/// When used as part
