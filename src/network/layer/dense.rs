79810ac9599332b0c590b8b678bee10429911e2f
whynoodle <119311098+whynoodle@users.noreply.github.com>
2021-07-20 08:12:45 +0000

    Test cases

--- src/network/layer/dense.rs
+++ src/network/layer/dense.rs
@@ -1,4 +1,10 @@
 use crate::network::layer::Layer;
 use crate::network::optimizer::Optimizer;
 use ndarray::{Array, Array1, Array2, ArrayD, Axis, Ix1, Ix2};
-use ndarray_rand::rand_distr::Normal; //{StandardNo
+use ndarray_rand::rand_distr::Normal; //{StandardNormal,Normal}; //not getting Standardnormal to work. should be better & faster
+use ndarray_rand::RandomExt;
+
+/// A dense (also called fully connected) layer.
+pub struct DenseLayer {
+    input_dim: usize,
+    outpu
