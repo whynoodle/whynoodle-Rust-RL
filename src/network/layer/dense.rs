c836360e9823bfbb49314d4af5e370ed56bab024
whynoodle <119311098+whynoodle@users.noreply.github.com>
2021-07-20 21:45:59 +0000

    Update vscode config

--- src/network/layer/dense.rs
+++ src/network/layer/dense.rs
@@ -7,4 +7,13 @@ use ndarray_rand::RandomExt;
 /// A dense (also called fully connected) layer.
 pub struct DenseLayer {
     input_dim: usize,
-    outpu
+    output_dim: usize,
+    learning_rate: f32,
+    weights: Array2<f32>,
+    bias: Array1<f32>,
+    net: Array2<f32>,
+    feedback: Array2<f32>,
+    batch_size: usize,
+    forward_passes: usize,
+    backward_passes: usize,
+    wei
