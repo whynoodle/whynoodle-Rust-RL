7cc376097d5b0a51f16bf08491843ab80820d195
whynoodle <119311098+whynoodle@users.noreply.github.com>
2022-04-07 19:43:31 +0000

    added newlines

--- src/network/layer/flatten.rs
+++ src/network/layer/flatten.rs
@@ -3,4 +3,13 @@ use ndarray::ArrayD;
 
 /// A flatten layer which turns higher dimensional input into a one dimension.
 ///
-/// A one dimensional in
+/// A one dimensional input remains unchanged.
+pub struct FlattenLayer {
+    input_ndim: usize,
+    input_shape: Vec<usize>,
+    batch_input_shape: Vec<usize>,
+    num_elements: usize,
+}
+
+impl FlattenLayer {
+    /// The input_shape is re
