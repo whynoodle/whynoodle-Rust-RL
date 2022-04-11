1556c76ee609d8c04ad694019f4091a28c1fdfc7
whynoodle <119311098+whynoodle@users.noreply.github.com>
2022-04-11 20:31:25 +0000

    0.15.2

--- src/network/layer/layer_trait.rs
+++ src/network/layer/layer_trait.rs
@@ -3,4 +3,9 @@ use ndarray::ArrayD;
 /// Layer Interface:  
 /// All layers passed to the neural network must implement this trait
 ///
-pub trait Layer: Send + Syn
+pub trait Layer: Send + Sync {
+    /// A unique String to identify the layer type, e.g. "Dense" or "Flatten"
+    ///
+    fn get_type(&self) -> String;
+
+    /// The number of trainable parameters in th
