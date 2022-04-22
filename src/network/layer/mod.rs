649dd6e170b6675187eee0b4ac0e9d363fc78770
whynoodle <119311098+whynoodle@users.noreply.github.com>
2022-04-22 07:01:41 +0000

    Bump ver

--- src/network/layer/mod.rs
+++ src/network/layer/mod.rs
@@ -5,4 +5,9 @@ mod flatten;
 //pub mod conv_test;
 mod layer_trait;
 
-/// This trait defines all functions which a
+/// This trait defines all functions which a layer has to implement to be used as a part of the neural network.
+pub use layer_trait::Layer;
+
+/// This layer implements a classical convolution layer.
+pub use convolution::ConvolutionLayer2D;
+/// This lay
