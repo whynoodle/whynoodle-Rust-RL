98bc197b1b2775b1239092ceeb04c02e7526019a
whynoodle <119311098+whynoodle@users.noreply.github.com>
2022-05-04 20:02:05 +0000

    Removed obsolete test

--- src/network/mod.rs
+++ src/network/mod.rs
@@ -1,1 +1,5 @@
-/// This submod
+/// This submodule offers multiple layer implementation.
+///
+/// The forward and backward functions have to accept and return data in the form ArrayD\<f32>.  
+/// Common activation functions are bundled under activation_layer.  
+pub 
