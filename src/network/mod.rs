5c5cb67fde959e7990e39d9f72e9f3e473994beb
whynoodle <119311098+whynoodle@users.noreply.github.com>
2022-05-07 09:55:03 +0000

    Version 0.1

--- src/network/mod.rs
+++ src/network/mod.rs
@@ -2,4 +2,9 @@
 ///
 /// The forward and backward functions have to accept and return data in the form ArrayD\<f32>.  
 /// Common activation functions are bundled under activation_layer.  
-pub 
+pub mod layer;
+
+/// This submodules bundles all neural network related functionalities.
+///
+/// A new neural network is created with new1d(..), new2d(..), or new3d(..).  
+/// For higher-dimensional input a new()
