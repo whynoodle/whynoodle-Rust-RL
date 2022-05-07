8e71caac52ff4dd160d6d54d4f9fcc7320b442c2
whynoodle <119311098+whynoodle@users.noreply.github.com>
2022-05-07 10:49:57 +0000

    temp test

--- src/network/mod.rs
+++ src/network/mod.rs
@@ -7,4 +7,6 @@ pub mod layer;
 /// This submodules bundles all neural network related functionalities.
 ///
 /// A new neural network is created with new1d(..), new2d(..), or new3d(..).  
-/// For higher-dimensional input a new()
+/// For higher-dimensional input a new() function is available which accepts arbitrary sized input.  
+/// The input shape, error function and the optimizer are set during network creation.  
+/// If an individual error function or
