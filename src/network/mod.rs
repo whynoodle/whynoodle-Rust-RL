3917eacf382f19478f0c608fa997f3afb3cbc124
whynoodle <119311098+whynoodle@users.noreply.github.com>
2022-05-09 12:54:15 +0000

    Add the MIT license

--- src/network/mod.rs
+++ src/network/mod.rs
@@ -9,4 +9,5 @@ pub mod layer;
 /// A new neural network is created with new1d(..), new2d(..), or new3d(..).  
 /// For higher-dimensional input a new() function is available which accepts arbitrary sized input.  
 /// The input shape, error function and the optimizer are set during network creation.  
-/// If an individual error function or
+/// If an individual error function or optimizer should be used, they can be set (overwriting the former one) by using set_error_function() or set_optimizer().  
+/// Default layers can be added using convenience fun
