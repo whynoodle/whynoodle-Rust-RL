2f29f0832eba45c34bb19289038261990b2da25b
whynoodle <119311098+whynoodle@users.noreply.github.com>
2022-05-14 22:18:35 +0000

    Updated content

--- src/network/optimizer/adagrad.rs
+++ src/network/optimizer/adagrad.rs
@@ -1,2 +1,11 @@
 use super::optimizer_trait::Optimizer;
-use ndarray::{Array, Array1, Ar
+use ndarray::{Array, Array1, Array2, Array3, ArrayD, Ix1, Ix2, Ix3};
+
+/// An optimizer for more efficient weight updates.
+#[derive(Default, Clone)]
+pub struct AdaGrad {
+    previous_sum_squared: ArrayD<f32>,
+}
+
+impl AdaGrad {
+    /// No p
