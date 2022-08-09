522b9ef3bedfc5fc7772a85561b040ad5de81809
whynoodle <119311098+whynoodle@users.noreply.github.com>
2022-08-09 09:05:44 +0000

    Latest log

--- src/network/optimizer/adam.rs
+++ src/network/optimizer/adam.rs
@@ -1,4 +1,15 @@
 use super::optimizer_trait::Optimizer;
 use ndarray::{Array, Array1, Array2, Array3, ArrayD, Ix1, Ix2, Ix3};
 
-/// An optimizer for more efficient weight up
+/// An optimizer for more efficient weight updates.
+#[derive(Clone)]
+pub struct Adam {
+    previous_sum: ArrayD<f32>,
+    previous_sum_squared: ArrayD<f32>,
+    beta1: f32,
+    beta2: f32,
+    t: f32,
+}
+
+impl Default for Adam {
+    fn default()
