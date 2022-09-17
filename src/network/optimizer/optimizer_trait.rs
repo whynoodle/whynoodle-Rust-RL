19238e37bfc0686bea79d73d69090ef6c32ca050
whynoodle <119311098+whynoodle@users.noreply.github.com>
2022-09-17 16:40:07 +0000

    Updated README.txt.

--- src/network/optimizer/optimizer_trait.rs
+++ src/network/optimizer/optimizer_trait.rs
@@ -3,4 +3,8 @@ use ndarray::{Array1, Array2, Array3, ArrayD};
 /// A trait defining functions to alter the weight and bias updates before they are applied.
 ///
 /// All neural network layers are expected to call the coresponding functions after calculating the deltas   
-/// and only to apply the results of these functi
+/// and only to apply the results of these functions to update their weights or biases.
+pub trait Optimizer: Send + Sync {
+    ///
+    fn set_input_shape(&mut self, shape: Vec<usize>);
+    /// Returns a string identifying the specific optimizer type. Examples are "Adam", "Momentum", 
