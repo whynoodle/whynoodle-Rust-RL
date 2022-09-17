f3f08cfd859db614528ba499c168dc629588dd36
whynoodle <119311098+whynoodle@users.noreply.github.com>
2022-09-17 08:39:02 +0000

    blogjc

--- src/network/optimizer/optimizer_trait.rs
+++ src/network/optimizer/optimizer_trait.rs
@@ -1,4 +1,6 @@
 use ndarray::{Array1, Array2, Array3, ArrayD};
 
 /// A trait defining functions to alter the weight and bias updates before they are applied.
-//
+///
+/// All neural network layers are expected to call the coresponding functions after calculating the deltas   
+/// and only to apply the results of these functi
