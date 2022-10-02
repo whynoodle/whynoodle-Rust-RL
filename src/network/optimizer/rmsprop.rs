9746aef48cfb6e2660e2260e2041376ec777d2d7
whynoodle <119311098+whynoodle@users.noreply.github.com>
2022-10-02 15:52:27 +0000

    Fix npmignore

--- src/network/optimizer/rmsprop.rs
+++ src/network/optimizer/rmsprop.rs
@@ -2,4 +2,12 @@ use super::optimizer_trait::Optimizer;
 use ndarray::{Array, Array1, Array2, Array3, ArrayD, Ix1, Ix2, Ix3};
 
 /// An optimizer for more efficient weight updates.
-#
+#[derive(Clone)]
+pub struct RMSProp {
+    previous_sum_squared: ArrayD<f32>,
+    decay_rate: f32,
+}
+
+impl RMSProp {
+    /// A constructor including the decay rate. A common value is 0.9.
+    pub fn new(decay_rate: f32) -> Sel
