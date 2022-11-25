00541d4b26e233838b32186730d52ae2c66d513f
whynoodle <119311098+whynoodle@users.noreply.github.com>
2022-11-25 22:58:00 +0000

    ch15

--- src/network/tests.rs
+++ src/network/tests.rs
@@ -98,3 +98,81 @@ mod MLP {
             let current_input1 = input.index_axis(Axis(0), pos1).into_owned();
             let current_input2 = input.index_axis(Axis(0), pos2).into_owned();
             let current_fb1 = feedback.index_axis(Axis(0), pos1).into_owned();
+            let current_fb2 = feedback.index_axis(Axis(0), pos2).into_owned();
+            println!("cfb1: {}", current_fb1);
+            let mut current_fb = Array::zeros((6, 1));
+            current_fb.index_axis_mut(Axis(0), 0).assign(&current_fb1);
+            current_fb.index_axis_mut(Axis(0), 1).assign(&current_fb2);
+            let mut current_input = Array::zeros((6, 2));
+            current_input
+                .index_axis_mut(Axis(0), 0)
+                .assign(&current_input1);
+            current_input
+                .index_axis_mut(Axis(0), 1)
+                .assign(&current_input2);
+            nn.train(current_input.into_dyn(), current_fb.into_dyn());
+        }
+    }
+
+    #[test]
+    fn test_entire_batch_input() {
+        //nn: &mut NeuralNetwork, num: usize, input: &Array4<f32>, fb: &Array2<f32>) {
+        let input = array![[0., 0.], [0., 1.], [1., 0.], [1., 1.], [1., 1.], [1., 1.]]; // AND
+        let feedback = array![[0.], [0.], [0.], [1.], [1.], [1.]]; //AND work ok with 200k examples (10 and 01 are classified correctly, but close to 0.5)
+        let mut nn = new(2, 6, 0.1);
+        nn.train(input.into_dyn(), feedback.into_dyn());
+    }
+
+    #[test]
+    fn clone_nn() {
+        let input = array![[0., 0.], [0., 1.], [1., 0.], [1., 1.], [1., 1.], [1., 1.]]; // AND
+        let feedback = array![[0.], [0.], [0.], [1.], [1.], [1.]]; //AND work ok with 200k examples (10 and 01 are classified correctly, but close to 0.5)
+        let mut nn = new(2, 6, 0.1);
+        train(&mut nn, 1_000, &input, &feedback);
+        test(
+            nn.clone(),
+            input.clone(),
+            feedback.clone(),
+            "copy_init_failed".to_string(),
+        );
+        let nn_clone = nn.clone();
+        test(nn_clone, input, feedback, "clone_failed".to_string());
+    }
+
+    #[test]
+    fn or() {
+        let input = array![[0., 0.], [0., 0.], [0., 0.], [0., 1.], [1., 0.], [1., 1.]]; // OR
+        let feedback = array![[0.], [0.], [0.], [1.], [1.], [1.]]; //OR works great with 200k examples
+        let mut nn = new(2, 6, 0.1);
+        train(&mut nn, 5000, &input, &feedback);
+        test(nn, input, feedback, "or".to_string());
+    }
+
+    #[test]
+    fn not() {
+        let input = array![[0.], [1.]];
+        let feedback = array![[1.], [0.]]; // NOT works great with 200k examples
+        let mut nn = new(1, 1, 0.1);
+        train(&mut nn, 500, &input, &feedback);
+        test(nn, input, feedback, "not".to_string());
+    }
+
+    #[test]
+    fn first() {
+        let input = array![[0., 0.], [0., 1.], [1., 0.], [1., 1.]]; // FIRST
+        let feedback = array![[0.], [0.], [1.], [1.]]; //First works good with 200k examples
+        let mut nn = new(2, 4, 0.1);
+        train(&mut nn, 500, &input, &feedback);
+        test(nn, input, feedback, "first".to_string());
+    }
+
+    #[test]
+    #[ignore]
+    fn xor() {
+        let input = array![[0., 0.], [0., 1.], [1., 0.], [1., 1.]];
+        let feedback = array![[0.], [1.], [1.], [0.]]; //XOR
+        let mut nn = new(2, 4, 0.1);
+        train(&mut nn, 30_000, &input, &feedback);
+        test(nn, input, feedback, "xor".to_string());
+    }
+}
---
+++ src/rl/agent/agent_trait.rs
@@ -0,0 +1,3 @@
+use ndarray::{Array1, Array2};
+
+/// A trait including all functions required to train the
