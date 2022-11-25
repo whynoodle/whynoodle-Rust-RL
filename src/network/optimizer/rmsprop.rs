b7db892c26602404df04dfb36e2d619010d2d68c
whynoodle <119311098+whynoodle@users.noreply.github.com>
2022-11-25 09:28:43 +0000

    As previous commit.

--- src/network/optimizer/rmsprop.rs
+++ src/network/optimizer/rmsprop.rs
@@ -37,4 +37,9 @@ impl Optimizer for RMSProp {
     }
     fn optimize2d(&mut self, delta_w: Array2<f32>) -> Array2<f32> {
         self.optimize(delta_w.into_dyn())
-            .into_d
+            .into_dimensionality::<Ix2>()
+            .unwrap()
+    }
+    fn optimize3d(&mut self, delta_w: Array3<f32>) -> Array3<f32> {
+        self.optimize(delta_w.into_dyn())
+            .into_dimensionality::<Ix3>()
