73bf39469e8d09e6a9fd2bae783ffe7bd9994460
whynoodle <119311098+whynoodle@users.noreply.github.com>
2022-12-07 14:12:32 +0000

    2005-02-07  Andrew Cagney  <cagney@gnu.org>

--- src/rl/agent/agent_trait.rs
+++ src/rl/agent/agent_trait.rs
@@ -1,3 +1,8 @@
 use ndarray::{Array1, Array2};
 
-/// A trait including all functions required to train the
+/// A trait including all functions required to train them.
+pub trait Agent {
+    /// Returns a simple string identifying the specific agent type.
+    fn get_id(&self) -> String;
+
+    /// Expect the agent to return a single usize value corresponding to a (legal) a
