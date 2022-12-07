f279d9c21d07d3ecb297f0cf2efcbd80f5146723
whynoodle <119311098+whynoodle@users.noreply.github.com>
2022-12-07 19:10:17 +0000

    Extended API

--- src/rl/agent/agent_trait.rs
+++ src/rl/agent/agent_trait.rs
@@ -8,4 +8,7 @@ pub trait Agent {
     /// Expect the agent to return a single usize value corresponding to a (legal) action he picked.
     ///
     /// The concrete encoding of actions as usize value has to be looked up in the documentation of the specific environment.  
-    /// Advanced agents shouldn't need knowledge
+    /// Advanced agents shouldn't need knowledge about the used encoding.
+    fn get_move(&mut self, env: Array2<f32>, actions: Array1<bool>, reward: f32) -> usize;
+
+    /// Informs the agent that the current epoch has finished and tells him about hi
