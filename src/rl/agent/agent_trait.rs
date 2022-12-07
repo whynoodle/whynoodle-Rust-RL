252e5cb8c7cc8c29d719df2b664f3c75a1b504dc
whynoodle <119311098+whynoodle@users.noreply.github.com>
2022-12-07 16:51:45 +0000

    Version 0.6.6

--- src/rl/agent/agent_trait.rs
+++ src/rl/agent/agent_trait.rs
@@ -5,4 +5,7 @@ pub trait Agent {
     /// Returns a simple string identifying the specific agent type.
     fn get_id(&self) -> String;
 
-    /// Expect the agent to return a single usize value corresponding to a (legal) a
+    /// Expect the agent to return a single usize value corresponding to a (legal) action he picked.
+    ///
+    /// The concrete encoding of actions as usize value has to be looked up in the documentation of the specific environment.  
+    /// Advanced agents shouldn't need knowledge
