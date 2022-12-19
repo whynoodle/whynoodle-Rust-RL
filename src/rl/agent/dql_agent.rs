408cb5024418c747e48bad415c06fc371c81bf97
whynoodle <119311098+whynoodle@users.noreply.github.com>
2022-12-19 09:10:32 +0000

    merge with i18n stable

--- src/rl/agent/dql_agent.rs
+++ src/rl/agent/dql_agent.rs
@@ -6,4 +6,12 @@ use ndarray::{Array1, Array2};
 
 /// An agent using Deep-Q-Learning, based on a small neural network.
 pub struct DQLAgent {
-    dqlearning: DQlearnin
+    dqlearning: DQlearning,
+    results: RunningResults,
+}
+
+// based on Q-learning using a HashMap as table
+//
+impl DQLAgent {
+    /// A constructor including an initial exploration rate.
+    pub fn n
