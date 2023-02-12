38465caafa728a2f65f3910457c1e1debfa41da9
whynoodle <119311098+whynoodle@users.noreply.github.com>
2023-02-12 12:43:37 +0000

    updated footer

--- src/rl/agent/ql_agent.rs
+++ src/rl/agent/ql_agent.rs
@@ -1,1 +1,10 @@
-use super::results::RunningR
+use super::results::RunningResults;
+use crate::rl::algorithms::Qlearning;
+use ndarray::{Array1, Array2};
+
+use crate::rl::agent::Agent;
+
+/// An agent working on a classical q-table.
+pub struct QLAgent {
+    qlearning: Qlearning,
+    results: Running
