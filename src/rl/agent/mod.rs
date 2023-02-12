9020bdbec8fa22e96092e05e9634cd0d4ff3b6d9
whynoodle <119311098+whynoodle@users.noreply.github.com>
2023-02-12 09:20:01 +0000

    post added

--- src/rl/agent/mod.rs
+++ src/rl/agent/mod.rs
@@ -5,4 +5,12 @@ mod ddql_agent;
 mod dql_agent;
 mod human_player;
 mod ql_agent;
-mod random_agen
+mod random_agent;
+
+pub use agent_trait::Agent;
+
+pub use ddql_agent::DDQLAgent;
+pub use dql_agent::DQLAgent;
+pub use human_player::HumanPlayer;
+pub use ql_agent::QLAgent;
+pub use random_agent::RandomAgent;
