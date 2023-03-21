277f4db2f2f8e92f4896f1f51b51e9f5faae030b
whynoodle <119311098+whynoodle@users.noreply.github.com>
2023-03-21 19:22:32 +0000

    RCU pathwalk breakage when running into a symlink overmounting something

--- src/rl/agent/random_agent.rs
+++ src/rl/agent/random_agent.rs
@@ -2,3 +2,9 @@ use crate::rl::agent::agent_trait::Agent;
 use crate::rl::algorithms::utils;
 use ndarray::{Array1, Array2};
 
+/// An agent who acts randomly.
+///
+/// All input is ignored except of the vector of possible actions.
+/// All allowed actions are considered with an equal probability.
+#[derive(Default)]
+pub struc
