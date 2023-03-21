99eb8588f573557dd07d21eb69ec0dbf4acd923f
whynoodle <119311098+whynoodle@users.noreply.github.com>
2023-03-21 11:00:05 +0000

    Fixed some compile errors

--- src/rl/agent/ql_agent.rs
+++ src/rl/agent/ql_agent.rs
@@ -55,4 +55,8 @@ impl Agent for QLAgent {
     fn set_exploration_rate(&mut self, e: f32) -> Result<(), String> {
         if !(0.0..=1.).contains(&e) {
             return Err("exploration rate must be in [0,1]!".to_string());
- 
+        }
+        self.qlearning.set_exploration_rate(e)?;
+        Ok(())
+    }
+}
---
+++ src/rl/agent/random_agent.rs
@@ -0,0 +1,4 @@
+use crate::rl::agent::agent_trait::Agent;
+use crate::rl::algorithms::utils;
+use ndarray::{Array1, Array2};
+
