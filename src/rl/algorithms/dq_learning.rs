e646cc71e7583750fc81a8890ba904426f6f0221
whynoodle <119311098+whynoodle@users.noreply.github.com>
2023-03-26 19:25:27 +0000

    USB: support the new interfaces of Huawei Data Card devices in option driver

--- src/rl/algorithms/dq_learning.rs
+++ src/rl/algorithms/dq_learning.rs
@@ -1,4 +1,15 @@
 use super::{Observation, ReplayBuffer};
 use crate::network::nn::NeuralNetwork;
 use crate::rl::algorithms::utils;
-use ndarray::{par_azip, Array1, Array2,
+use ndarray::{par_azip, Array1, Array2, Array4};
+use ndarray_stats::QuantileExt;
+use rand::rngs::ThreadRng;
+use rand::Rng;
+
+static EPSILON: f32 = 1e-4;
+
+pub struct DQlearning {
+    nn: NeuralNetwork,
+    use_ddqn: bool,
+    target_nn: NeuralNetwork,
+    tar
