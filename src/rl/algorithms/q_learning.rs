eb68f20fa8f5300afad97a36e4d86ca046cf6a66
whynoodle <119311098+whynoodle@users.noreply.github.com>
2023-05-16 19:24:30 +0000

    Screenshot.

--- src/rl/algorithms/q_learning.rs
+++ src/rl/algorithms/q_learning.rs
@@ -3,4 +3,12 @@ use crate::rl::algorithms::utils;
 use ndarray::{Array1, Array2};
 use rand::rngs::ThreadRng;
 use rand::Rng;
-use std::
+use std::collections::HashMap;
+
+#[allow(dead_code)]
+pub struct Qlearning {
+    exploration: f32,
+    learning_rate: f32,
+    discount_factor: f32,
+    scores: HashMap<(String, usize), f32>, // (State,Action), reward
+    repla
