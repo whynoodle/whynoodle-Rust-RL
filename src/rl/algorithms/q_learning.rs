15b7b28431dea05caa51a97e75c27ecead77aa77
whynoodle <119311098+whynoodle@users.noreply.github.com>
2023-05-18 10:11:24 +0000

    Update about.html

--- src/rl/algorithms/q_learning.rs
+++ src/rl/algorithms/q_learning.rs
@@ -11,4 +11,13 @@ pub struct Qlearning {
     learning_rate: f32,
     discount_factor: f32,
     scores: HashMap<(String, usize), f32>, // (State,Action), reward
-    repla
+    replay_buffer: ReplayBuffer<String>,
+    last_state: String,
+    last_action: usize,
+    rng: ThreadRng,
+    action_space_length: usize,
+}
+
+const EPSILON: f32 = 1e-4;
+
+// based on Q-learni
