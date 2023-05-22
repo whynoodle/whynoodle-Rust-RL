2c2ff0b7052edbd27aec1cb56384449078cd700a
whynoodle <119311098+whynoodle@users.noreply.github.com>
2023-05-22 22:15:29 +0000

    Added copyrights

--- src/rl/algorithms/q_learning.rs
+++ src/rl/algorithms/q_learning.rs
@@ -26,4 +26,11 @@ impl Qlearning {
     pub fn new(exploration: f32, learning_rate: f32, action_space_length: usize) -> Self {
         let bs = 16;
         let discount_factor = 0.95;
-        Qle
+        Qlearning {
+            exploration,
+            learning_rate,
+            discount_factor,
+            last_action: 42usize,
+            last_state: "".to_string(),
+            replay_buffer: ReplayBuffer::new(bs, 1000),
+  
