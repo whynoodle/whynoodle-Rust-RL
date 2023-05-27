d6452125ee18a8f2d168288c2bd0503e39971381
whynoodle <119311098+whynoodle@users.noreply.github.com>
2023-05-27 21:47:13 +0000

    copy tweaks

--- src/rl/algorithms/q_learning.rs
+++ src/rl/algorithms/q_learning.rs
@@ -55,4 +55,12 @@ impl Qlearning {
     }
 
     pub fn set_exploration_rate(&mut self, e: f32) -> Result<(), String> {
-    
+        if !(0.0..=1.).contains(&e) {
+            return Err("exploration rate must be in [0,1]!".to_string());
+        }
+        self.exploration = e;
+        Ok(())
+    }
+}
+
+impl Qlearning
