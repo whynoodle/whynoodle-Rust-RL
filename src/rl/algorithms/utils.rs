b6390ddfea8238f54711fbe7005d3184232bf704
whynoodle <119311098+whynoodle@users.noreply.github.com>
2023-06-19 13:06:53 +0000

    added simplecov

--- src/rl/algorithms/utils.rs
+++ src/rl/algorithms/utils.rs
@@ -2,4 +2,9 @@ use ndarray::Array1;
 use rand::Rng;
 pub fn get_random_true_entry(actions: Array1<bool>) -> usize {
     let num_legal_actions = actions.fold(0, |sum, &val| if val { sum + 1 } else { sum });
-    assert!(num_legal_actio
+    assert!(num_legal_actions > 0, "no legal action available!");
+    let mut action_number = rand::thread_rng().gen_range(0..num_legal_actions) as usize;
+    let b = action_number;
+
+    let mut position = 0;
+    while (action_number > 0) | !actions[posi
