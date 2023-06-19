9b6fe9d031d0c8ad73218d6a5be99923eba23a4a
whynoodle <119311098+whynoodle@users.noreply.github.com>
2023-06-19 10:22:11 +0000

    Link to documentation

--- src/rl/algorithms/utils.rs
+++ src/rl/algorithms/utils.rs
@@ -1,2 +1,5 @@
 use ndarray::Array1;
-use rand::Rng
+use rand::Rng;
+pub fn get_random_true_entry(actions: Array1<bool>) -> usize {
+    let num_legal_actions = actions.fold(0, |sum, &val| if val { sum + 1 } else { sum });
+    assert!(num_legal_actio
