57f1b383f733e74b6563e1695b55bf51a72e211a
whynoodle <119311098+whynoodle@users.noreply.github.com>
2023-06-21 17:17:36 +0000

    SourceFormat Enforcement

--- src/rl/algorithms/utils.rs
+++ src/rl/algorithms/utils.rs
@@ -7,4 +7,12 @@ pub fn get_random_true_entry(actions: Array1<bool>) -> usize {
     let b = action_number;
 
     let mut position = 0;
-    while (action_number > 0) | !actions[posi
+    while (action_number > 0) | !actions[position] {
+        if actions[position] {
+            action_number -= 1;
+        }
+        position += 1;
+    }
+    assert!(
+        actions[position],
+        "randomly picked illegal move! {:} {} 
