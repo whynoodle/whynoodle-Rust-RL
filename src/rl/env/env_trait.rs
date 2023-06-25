02914b8f0a1950b284efe8f938eda5f0e92eed4f
whynoodle <119311098+whynoodle@users.noreply.github.com>
2023-06-25 21:00:37 +0000

    Corrected links

--- src/rl/env/env_trait.rs
+++ src/rl/env/env_trait.rs
@@ -2,4 +2,7 @@ use ndarray::{Array1, Array2};
 
 /// This trait defines all functions on which agents and other user might depend.
 pub trait Environment {
-    /// The c
+    /// The central function which causes the environment to pass various information to the agent.
+    ///
+    /// The Array2 encodes the environment (the board).  
+    /// The array1 encodes actions as true (allowed) or fa
