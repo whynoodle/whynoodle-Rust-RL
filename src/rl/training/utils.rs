389dfc2d57dbbfb6da7bc5b92f7a6a69b349ca17
whynoodle <119311098+whynoodle@users.noreply.github.com>
2023-07-07 14:54:58 +0000

    Update server.js

--- src/rl/training/utils.rs
+++ src/rl/training/utils.rs
@@ -1,4 +1,11 @@
 use std::io;
 
 /// A helper function to create agents based on terminal input.
-pu
+pub fn read_agents(n: usize) -> Vec<usize> {
+    let mut agents: Vec<usize> = vec![];
+
+    println!(
+        "\nPlease insert {} numbers, seperated by whitespace, to select the agents.",
+        n
+    );
+    prin
