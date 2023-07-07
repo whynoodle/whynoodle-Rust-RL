8c574da3542e9ce6b57f0210d520196f33544a00
whynoodle <119311098+whynoodle@users.noreply.github.com>
2023-07-07 21:16:06 +0000

    multiple piface

--- src/rl/training/utils.rs
+++ src/rl/training/utils.rs
@@ -8,4 +8,8 @@ pub fn read_agents(n: usize) -> Vec<usize> {
         "\nPlease insert {} numbers, seperated by whitespace, to select the agents.",
         n
     );
-    prin
+    println!("(0 for ddql, 1 for dql, 2 for ql, 3 for random, 4 for human)");
+    let stdin = io::stdin();
+    loop {
+        let mut buffer = String::new();
+        stdin.read_line(&mut buffer).unwrap();
